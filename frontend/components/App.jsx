eeef81cb7632047ae4f65384236a791bf50d2751
Sarah Confrancisco <sarahconfrancisco@gmail.com>
2017-02-14 17:44:57 -0500

    Fix typo

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const App = ({ children }) => (
+const App = (props) => (
   <div>
     <header>
        <h1>Welp That's Dinner</h1>
     </header>
-    {children}
+    {props.children}
   </div>
 );
 
