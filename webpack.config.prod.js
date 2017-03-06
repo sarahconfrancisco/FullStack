3faf72f68d8e94937c664c55674166cac86c2437
Sarah Confrancisco <sarahconfrancisco@gmail.com>
2017-03-06 17:59:01 -0500

    Remove React/Redux Minification Warnings

---
+++ webpack.config.prod.js
@@ -0,0 +1,40 @@
+let path = require("path");
+let webpack = require("webpack");
+
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/welp.jsx",
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ["*", ".js", ".jsx" ]
+  }
+};
