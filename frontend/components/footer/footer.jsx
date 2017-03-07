bd1f2b9a2a11c3fbb9c8a10afb9fccbea1d7a706
Sarah Confrancisco <sarahconfrancisco@gmail.com>
2017-03-07 08:33:00 -0500

    Add a favicon and footer links

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -7,13 +7,19 @@ const Footer = (props) => {
 
         <li>
           <ul>
-            <li className="about-me">About Me</li>
+            <li><h5>Contact Me</h5></li>
+            <li><a href="mailto:sarahconfrancisco@gmail.com">sarahconfrancisco@gmail.com</a></li>
+            <li>201-316-6607</li>
           </ul>
         </li>
 
-      <li>      
+      <li>
         <ul>
-          <li className="my-links">My Links</li>
+          <li className="my-links"><h5>My Links</h5></li>
+          <li><a href="https://sarahconfrancisco.github.io/">Personal Website</a></li>
+          <li><a href="https://github.com/sarahconfrancisco">GitHub</a></li>
+          <li><a href="https://www.linkedin.com/in/sarah-confrancisco/">LinkedIn</a></li>
+          <li><a href="https://angel.co/sarah-confrancisco">Angel List</a></li>
         </ul>
       </li>
 
