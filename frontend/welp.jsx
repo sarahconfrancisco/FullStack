a22feda93ad6b4692e56f93252b46523578659f5
Sarah Confrancisco <sarahconfrancisco@gmail.com>
2017-03-29 19:19:10 -0400

    remove unnecessary imports

--- frontend/welp.jsx
+++ frontend/welp.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import Root from './components/root';
 import configureStore from './store/store';
-import { addReview } from './util/review_api_util';
-import { showRestaurant } from './util/restaurant_api_util';
-import { fetchSearchRestaurants } from './util/search_api_util';
 
 document.addEventListener("DOMContentLoaded", () => {
-  let preloadedState
+  let store;
   if (window.currentUser){
     preloadedState = {currentUser: window.currentUser};
-  }
-  const store = configureStore(preloadedState);
+		store = configureStore(preloadedState);
+  } else {
+		store = configureStore();
+	}
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);
 })
