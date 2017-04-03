b1d89a4874d66af51ddbf1f33ee2a7085ee97b76
Sarah Confrancisco <sarahconfrancisco@gmail.com>
2017-04-03 11:48:05 -0400

    Migrate images table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170310180307) do
+ActiveRecord::Schema.define(version: 20170403134950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.integer  "user_id",            null: false
+    t.integer  "restaurant_id",      null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
+  end
+
+  add_index "images", ["restaurant_id"], name: "index_images_on_restaurant_id", using: :btree
+  add_index "images", ["user_id", "restaurant_id"], name: "index_images_on_user_id_and_restaurant_id", using: :btree
+  add_index "images", ["user_id"], name: "index_images_on_user_id", using: :btree
 
   create_table "pg_search_documents", force: :cascade do |t|
     t.text     "content"
