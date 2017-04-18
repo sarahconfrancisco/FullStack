74e120cd4b72651acf5050393b14bc2c07c92c81
Sarah Confrancisco <sarahconfrancisco@gmail.com>
2017-04-18 12:39:43 -0400

    Add image routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,11 @@
 Rails.application.routes.draw do
-
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
+    
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
     resources :restaurants, only: [:create, :show, :update, :index]
     resources :reviews, only: [:create, :show, :update, :index]
-		resources :images, except: [:new, :edit]
+	resources :images, except: [:new, :edit]
   end
   root "static_pages#root"
 
