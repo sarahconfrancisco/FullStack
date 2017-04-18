a6cfdd09b29863fd52e1209ef8933f3d59b16239
Sarah Confrancisco <sarahconfrancisco@gmail.com>
2017-04-18 12:47:29 -0400

    create image slice of state

---
+++ frontend/reducers/image_reducer.js
@@ -0,0 +1,14 @@
+import { RECEIVE_IMAGES } from '../actions/image_actions';
+import { merge } from 'lodash';
+
+const ImageReducer = (oldState = {}, action) => {
+  Object.freeze(oldState);
+  switch (action.type) {
+    case "RECEIVE_IMAGES":
+      return action.images;
+    default:
+      return oldState;
+  }
+};
+
+export default ImageReducer;
