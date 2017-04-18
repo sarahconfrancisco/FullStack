12efdf34807a9439748cff83e51fd98c5344bf40
Sarah Confrancisco <sarahconfrancisco@gmail.com>
2017-04-18 12:47:55 -0400

    add a slice to loading state pertaining to index photos

--- frontend/reducers/loading_reducer.js
+++ frontend/reducers/loading_reducer.js
@@ -1,17 +1,21 @@
 import { LOADING_A_RESTAURANT, RECEIVE_A_RESTAURANT } from '../actions/restaurant_actions';
 import { RECEIVE_RESULTS, LOADING_SEARCH } from '../actions/search_actions';
+import { RECEIVE_IMAGES, LOADING_IMAGES } from '../actions/image_actions';
 
-const initialState = { searchLoading: false, restaurantLoading: false };
+const initialState = { searchLoading: false, restaurantLoading: false, imageLoading: false };
 
 const LoadingReducer = ( state = initialState, action ) => {
   switch (action.type) {
     case RECEIVE_A_RESTAURANT:
     case RECEIVE_RESULTS:
+    case RECEIVE_IMAGES:
       return initialState;
     case LOADING_A_RESTAURANT:
       return Object.assign({}, state, { restaurantLoading: true });
     case LOADING_SEARCH:
       return Object.assign({}, state, { searchLoading: true });
+    case LOADING_IMAGES:
+      return Object.assign({}, state, { imageLoading: true })
     default:
       return state;
 
